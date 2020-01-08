235ad16bd78c1bf272baf50ae4696d426f4f07ab
Niklas Bender <niklas.c.bender@th-bingen.de>
2020-01-08 22:08:35 +0100

    REFACTORE: Use @RepositoryRestResource + some small refactore changes / additional configurations in application.properties

--- src/main/java/com/asys1920/carservice/repository/CarRepository.java
+++ src/main/java/com/asys1920/carservice/repository/CarRepository.java
@@ -3,10 +3,11 @@ package com.asys1920.carservice.repository;
 import com.asys1920.carservice.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CarRepository extends JpaRepository<Car, String> {
+@RepositoryRestResource(collectionResourceRel = "car", path = "car")
+public interface CarRepository extends JpaRepository<Car, Long> {
 
     Car getCarById(@Param("id") Long id);
     Car getCarByName(@Param("name") String name);
