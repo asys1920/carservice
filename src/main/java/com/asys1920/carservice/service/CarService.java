990c14eb4bd4562cf2c5ea308dc92e2f676f39a8
AndreasKlar <andreas.klar@th-bingen.de>
2020-02-28 11:48:51 +0100

    Added Logging

--- src/main/java/com/asys1920/carservice/service/CarService.java
+++ src/main/java/com/asys1920/carservice/service/CarService.java
@@ -2,13 +2,17 @@ package com.asys1920.carservice.service;
 
 import com.asys1920.carservice.repository.CarRepository;
 import com.asys1920.model.Car;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
 public class CarService {
+    private static final Logger LOG = LoggerFactory.getLogger(CarService.class);
     private final CarRepository carRepository;
 
     public CarService(CarRepository carRepository) {
@@ -16,23 +20,33 @@ public class CarService {
     }
 
     public boolean carExists(Car car) {
+        LOG.trace(String.format("SERVICE %s %d initiated", "carExists", car.getId()));
         Optional<Car> foundCar = carRepository.findByName(car.getName());
         return foundCar.isPresent();
     }
 
     public Car createCar(Car car) {
+        LOG.trace(String.format("SERVICE %s %d initiated", "createCar",car.getId()));
         return carRepository.save(car);
     }
 
     public List<Car> getAllCars() {
+        LOG.trace(String.format("SERVICE %s initiated", "getAllCars"));
         return carRepository.findAll();
     }
 
     public Car getCar(long id) {
-        return carRepository.findById(id).get();
+        LOG.trace(String.format("SERVICE %s %d initiated", "getCar",id));
+        Optional<Car> car = carRepository.findById(id);
+        if(car.isPresent()){
+            return car.get();
+        }else{
+            throw new NoSuchElementException(String.format("No car with if %d is known", id));
+        }
     }
 
     public void deleteCar(long id) {
+        LOG.trace(String.format("SERVICE %s %d initiated", "deleteCar",id));
         carRepository.deleteById(id);
     }
 }
