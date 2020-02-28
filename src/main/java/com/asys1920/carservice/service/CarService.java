c3e618de81292f799d29fac4f5cb8de789e40945
AndreasKlar <andreas.klar@th-bingen.de>
2020-02-28 13:38:37 +0100

    added javadoc

--- src/main/java/com/asys1920/carservice/service/CarService.java
+++ src/main/java/com/asys1920/carservice/service/CarService.java
@@ -19,22 +19,42 @@ public class CarService {
         this.carRepository = carRepository;
     }
 
+    /**
+     * Checks if the given car exists in the repository
+     * @param car the car that gets checked
+     * @return if the car is in the repository
+     */
     public boolean carExists(Car car) {
         LOG.trace(String.format("SERVICE %s %d initiated", "carExists", car.getId()));
         Optional<Car> foundCar = carRepository.findByName(car.getName());
         return foundCar.isPresent();
     }
 
+    /**
+     * Saves a car in the repository
+     * @param car the car that gets saved
+     * @return the saved car
+     */
     public Car createCar(Car car) {
         LOG.trace(String.format("SERVICE %s %d initiated", "createCar",car.getId()));
         return carRepository.save(car);
     }
 
+    /**
+     * Retrieves all cars from the repository
+     * @return a list with all cars in the repository
+     */
     public List<Car> getAllCars() {
         LOG.trace(String.format("SERVICE %s initiated", "getAllCars"));
         return carRepository.findAll();
     }
 
+    /**
+     * Gets a car associated with the given id
+     * @param id the id of the car
+     * @return the car
+     * @throws NoSuchElementException gets thrown when there is no car known for the given id
+     */
     public Car getCar(long id) {
         LOG.trace(String.format("SERVICE %s %d initiated", "getCar",id));
         Optional<Car> car = carRepository.findById(id);
@@ -45,8 +65,18 @@ public class CarService {
         }
     }
 
+    /**
+     * Deletes a car from the repository
+     * @param id the id of the car that gets deleted
+     * @throws NoSuchElementException gets thrown when there is no car known with the given id
+     */
     public void deleteCar(long id) {
         LOG.trace(String.format("SERVICE %s %d initiated", "deleteCar",id));
-        carRepository.deleteById(id);
+        Optional<Car> car = carRepository.findById(id);
+        if(car.isPresent()){
+            carRepository.delete(car.get());
+        }else{
+            throw new NoSuchElementException(String.format("No car with if %d is known", id));
+        }
     }
 }
